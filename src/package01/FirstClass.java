e98a09885cc88e847a6646751aa807ebfa8a7550
suadahoxha <apple@apple-MacBook-Pro.local>
2020-06-21 15:20:47 -0400

    yeni properity

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -3,8 +3,10 @@ package package01;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Ilk commit from local repository");
+		System.out.println("Ilk commit from local repository111");
+		System.out.println("Ilk commit from local repository222");
 	}
 
 }
